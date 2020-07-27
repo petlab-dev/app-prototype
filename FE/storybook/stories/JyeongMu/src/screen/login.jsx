50b24ccdbb60f85356af0711b194d366f9f43334
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-28 06:44:04 +0900

    [#12] Combine states Delete color state and combine it to general state.

--- FE/storybook/stories/JyeongMu/src/screen/login.jsx
+++ FE/storybook/stories/JyeongMu/src/screen/login.jsx
@@ -15,28 +15,29 @@ import {
 
 export default function Login() {
   const [value, setValue] = useState('');
-  const [color, setColor] = useState({ button1: true, button2: false });
-  const [state, setState] = useState({ button: true, defualtMessage: 'Phone Number ex)01011112222' });
+  const [state, setState] = useState({
+    nextButton: true,
+    toggleButton: true,
+    defualtMessage: 'Phone Number ex)01011112222',
+  });
 
-  const { button, defualtMessage } = state;
+  const { nextButton, toggleButton, defualtMessage } = state;
 
   function isDisabled(event) {
     setValue(event);
     if (value.length > 1) {
-      setState({ ...state, button: false });
+      setState({ ...state, nextButton: false });
       return;
     }
-    setState({ ...state, button: true });
+    setState({ ...state, nextButton: true });
   }
 
   function handleChangeToPhoneButton() {
-    setColor({ button1: true, button2: false });
-    setState({ ...state, defualtMessage: 'Phone Number ex)01011112222' });
+    setState({ ...state, toggleButton: !toggleButton, defualtMessage: 'Phone Number ex)01011112222' });
   }
 
   function handleChangeToEmailButton() {
-    setColor({ button1: false, button2: true });
-    setState({ ...state, defualtMessage: 'Email Adrress ex)email@adress.com' });
+    setState({ ...state, toggleButton: !toggleButton, defualtMessage: 'Email Adrress ex)email@adress.com' });
   }
 
   return (
@@ -49,12 +50,12 @@ export default function Login() {
           <LoginSelectButton
             onClick={handleChangeToPhoneButton}
             text="Phone"
-            active={color.button1}
+            active={toggleButton}
           />
           <LoginSelectButton
             onClick={handleChangeToEmailButton}
             text="Email"
-            active={color.button2}
+            active={!toggleButton}
           />
         </Select>
         <TextInputWrap>
@@ -66,7 +67,7 @@ export default function Login() {
         <ButtonContainer>
           <Button
             title="Next"
-            disabled={button}
+            disabled={nextButton}
           />
         </ButtonContainer>
         <ClosingMent />
